45f62214f627de194f854e26b86551bc0b796cef
shivam vora <vorashivam0@gmail.com>
2023-04-13 09:02:18 +0530

    update code for modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,21 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import { useState } from 'react';
+import Joinlist from '@/components/Joinlist';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [isActive, setIsActive] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
 
   const handleClick = () => {
     setIsActive(current => !current);
   };
 
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-between">
       <nav className="bg-[#0D0D0D] fixed w-full z-20 top-0 left-0 h-[100px]">
         <div className="max-w-[1352px] flex flex-wrap items-center justify-between mx-auto p-4 py-7 h-full">
@@ -26,16 +30,24 @@ export default function Home() {
             />
           </a>
           <div className="flex lg:order-2">
-            <button type="button" className="join-waitlist-btn text-white font-unbounded font-normal lg:text-xl text-base px-3 py-2 text-center mr-3 lg:mr-0">Join Waitlist</button>
-            <button type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden" aria-controls="navbar-sticky" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            <button type="button" className="join-waitlist-btn text-white font-unbounded font-normal lg:text-xl text-base px-3 py-2 text-center mr-3 lg:mr-0"  onClick={() => {
+                    setShowModal(true);
+                }}>Join Waitlist</button>
+            <button type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden" aria-controls="navbar-sticky" aria-expanded="false" onClick={handleClick}>
+                <span className="sr-only">Open main menu</span>
+                {isActive ? (
+                  <svg fill="#fff" className="w-6 h-6" viewBox="-0.18 -0.18 0.72 0.72" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin">
+                    <path d="m0.219 0.177 0.106 -0.106A0.03 0.03 0 1 0 0.283 0.028L0.177 0.135 0.071 0.028A0.03 0.03 0 1 0 0.028 0.071l0.106 0.106L0.028 0.283a0.03 0.03 0 1 0 0.042 0.042l0.106 -0.106 0.106 0.106a0.03 0.03 0 1 0 0.042 -0.042L0.219 0.177z" />
+                  </svg>
+                  ) : (
+                  <svg className="w-6 h-6" aria-hidden="true" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                )}
             </button>
           </div>
-          <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+          <div className={`h-screen lg:h-auto bg-[#0D0D0D] items-center justify-between w-full lg:flex lg:w-auto lg:order-1 absolute z-[1] top-[100px] lg:z-auto lg:static left-0 transition-all ease-in duration-300 opacity-0 ${isActive ? 'opacity-100' : 'opacity-0 '} lg:opacity-100` }>
             <ul className="flex flex-col p-4 md:p-0 mt-4 text-xl lg:flex-row lg:space-x-6 lg:mt-0">
               <li>
-                <Link href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded lg:bg-transparent lg:p-0 font-sora font-normal" aria-current="page">Home</Link>
+                <Link href="#" className="navar-item block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:p-0 font-sora font-normal active" aria-current="page">Home</Link>
               </li>
               <li>
                 <Link href="#" className="block py-2 pl-3 pr-4 lg:p-0 text-white font-sora font-normal">Features</Link>
@@ -62,7 +74,9 @@ export default function Home() {
           <div className='lg:pr-7 pr-0 max-w-[612px]'>
             <h1 className='lg:text-[56px] text-[46px] leading-[59px] lg:leading-[69px] text-white font-unbounded font-semibold'>Effortless AI-Powered <span className='gradient-text'>Course Building</span></h1>
             <p className='lg:text-2xl text-xl leading-[29px] text-white my-6 font-inter font-normal'>Empowering Educators, Revolutionizing Learning: The Future of Course Creation</p>
-            <button type="button" className='join-waitlist-btn font-unbounded font-normal text-white font-medium lg:text-[28px] text-[22px] px-6 py-3 text-center mr-3 md:mr-0'>Join Waitlist Now</button>
+            <button type="button" className='join-waitlist-btn font-unbounded font-normal text-white font-medium lg:text-[28px] text-[22px] px-6 py-3 text-center mr-3 md:mr-0' onClick={() => {
+                    setShowModal(true);
+                }}>Join Waitlist Now</button>
           </div>
           <div className='lg:pl-7 pl-0 lg:max-w-[589px] max-w-[489px] lg:mb-0 mb-3 w-full lg:max-h-[424px] max-h-[350px] h-full rounded-[64px] overflow-hidden relative flex items-center justify-center'>
             <Image
@@ -341,6 +355,45 @@ export default function Home() {
           </div>
         </div>
       </section>
+      {showModal && <Joinlist setOpenModal={setShowModal} />}
     </main>
+    {/* <div className='login-container w-full h-screen relative flex items-center justify-center p-4'>
+      <div className='bg-[#151515] md:py-14 py-4 md:px-[42px] px-4 max-w-[536px] w-full text-center' style={{ boxShadow: '0px 0px 10px rgba(76, 72, 255, 0.04)' }}>
+        <h1 className='md:text-[42px] md:leading-[52px] leading-[42px] text-3xl text-white font-unbounded font-semibold md:mb-6 mb-3'>Login</h1>
+        <p className='md:text-2xl text-xl md:leading-[30px] leading-6 text-white md:mb-6 mb-3 font-sora font-normal'>Login using one of your social accounts</p>
+        <button type='button' className='btn active font-unbounded font-bold relative w-full flex items-center justify-center md:py-5 py-3 md:px-6 px-3 bg-[#111111] text-white md:text-xl text-base mb-3'>
+          <Image
+            src="/assets/images/google-icon.svg"
+            alt="logo"
+            width={24}
+            height={24}
+            className='mr-2 absolute left-5'
+          />
+          Log in using Google
+        </button>
+        <button type='button' className='btn font-unbounded font-bold relative w-full flex items-center justify-center md:py-5 py-3 md:px-6 px-3 bg-[#111111] text-white md:text-xl text-base mb-3'>
+          <Image
+            src="/assets/images/apple-icon.svg"
+            alt="logo"
+            width={24}
+            height={24}
+            className='mr-2 absolute left-5'
+          />
+          Log in using Apple
+        </button>
+        <button type='button' className='btn font-unbounded font-bold relative w-full flex items-center justify-center md:py-5 py-3 md:px-6 px-3 bg-[#111111] text-white md:text-xl text-base mb-3'>
+          <Image
+            src="/assets/images/github-icon.svg"
+            alt="logo"
+            width={24}
+            height={24}
+            className='mr-2 absolute left-5'
+          />
+          Log in using GitHub
+        </button>
+        <p className='md:mt-6 mt-3 text-white md:text-xl text-base font-sora font-normal'>Powered by <span className='font-sora font-bold'>ainsoft™️</span></p>
+      </div>
+    </div> */}
+    </>
   )
 }
